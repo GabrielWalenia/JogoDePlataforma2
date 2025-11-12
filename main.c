5a9ec65cc36c9883fab2c33765b7eecd658e7d6e
Gabriel Antonio Walenia <gabriel.walenia@gmail.com>
2025-11-12 11:18:09 -0300

    sprites

--- main.c
+++ main.c
@@ -33,19 +33,6 @@ gaw24@h41:~$ echo $((718/12))
 #define X_SCREEN 1280
 #define Y_SCREEN 720
 
-void show_menu(ALLEGRO_DISPLAY *disp, ALLEGRO_EVENT_QUEUE *queue, ALLEGRO_EVENT event){
-    while(1){
-        if(event.type == 30){
-            al_clear_to_color(al_map_rgb(39, 104, 88));
-            al_draw_text(al_create_builtin_font(), al_map_rgb(0, 0, 0), X_SCREEN / 2, Y_SCREEN / 2, ALLEGRO_ALIGN_CENTRE, "Hello!" );
-            al_flip_display();
-        }else if(event.type == ALLEGRO_KEY_ENTER){
-            break;
-        }
-        else if(event.type == 42)
-            exit(1);
-    }
-}
 
 void update_position(personagem *player_1){
     if (player_1->controle->left){
@@ -63,11 +50,23 @@ void update_position(personagem *player_1){
 }
 
 int main(){
+
+
+    // -----------------------------------
+    // Iniciação componentes
     al_init();
     al_install_keyboard();
     // al_init_ttf_addon();
     al_init_primitives_addon();
     al_init_image_addon();
+
+    // -----------------------------------
+    // Variáveis
+
+    bool menu = true;
+    float frame = 0.f;
+    int pos_x = 0, pos_y = 0;
+    int current_frame_y = 0;
 
     ALLEGRO_TIMER *timer = al_create_timer(1.0/30.0);
     ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
@@ -75,6 +74,7 @@ int main(){
     ALLEGRO_DISPLAY *disp = al_create_display(1280, 720);
 
 
+    // -----------------------------------
     
     // Imagens:
     // ALLEGRO_BITMAP *background = al_load_bitmap(BACKGROUND_FILE);;
@@ -96,26 +96,36 @@ int main(){
     ALLEGRO_EVENT event;
 
     al_start_timer(timer);
-    show_menu(disp, queue, event);
     
     while(1){
         al_wait_for_event(queue, &event);
 
         if(event.type == 30){
-            update_position(player_1);
-            // al_draw_bitmap(background,0,0,0);
-            //al_clear_to_color(al_map_rgb(39, 104, 88));
-            
-            al_draw_bitmap(background, 0, 0, 0);
-            al_draw_filled_rectangle(player_1->x-player_1->width/2, player_1->y-player_1->height/2, player_1->x+player_1->width/2, player_1->y+player_1->height/2, al_map_rgb(255, 0, 0));
-            al_draw_bitmap_region(personagem, 0, 59, 59, 59, player_1->x - player_1->width/2, player_1->y - player_1->height/2, 0);
-            al_flip_display();
-        }
+            if(menu){
+                al_clear_to_color(al_map_rgb(0, 0, 0));
+                al_draw_text(font, al_map_rgb(255, 0, 0), X_SCREEN/2, Y_SCREEN/2, 0, "PRESSIONE ENTER");
+                al_flip_display();
+            }else{
+                update_position(player_1);
+                // al_draw_bitmap(background,0,0,0);
+                //al_clear_to_color(al_map_rgb(39, 104, 88));
+
+                frame +=0.04f;
+                if(frame > 4){
+                    frame -= 4;
+                }
+                al_draw_bitmap(background, 0, 0, 0);
+                al_draw_filled_rectangle(player_1->x-player_1->width/2, player_1->y-player_1->height/2, player_1->x+player_1->width/2, player_1->y+player_1->height/2, al_map_rgb(255, 0, 0));
+                al_draw_bitmap_region(personagem, 0, 59 * (int) frame, 59, 59, player_1->x - player_1->width, player_1->y - player_1->height, 0);
+                al_flip_display();
+            }
+            }
         else if ((event.type == 10) || (event.type == 12)){
             if (event.keyboard.keycode == 82) joystick_left(player_1->controle);
             else if (event.keyboard.keycode == 83) joystick_right(player_1->controle);
             else if (event.keyboard.keycode == 84) joystick_up(player_1->controle);
             else if (event.keyboard.keycode == 85) joystick_down(player_1->controle);
+            else if(event.keyboard.keycode == ALLEGRO_KEY_ENTER) menu = false;
         }
 
         else if(event.type == 42)
@@ -126,6 +136,7 @@ int main(){
 
     al_destroy_bitmap(personagem);
     al_destroy_bitmap(background);
+
     al_destroy_font(font);
     al_destroy_display(disp);
     al_destroy_timer(timer);
